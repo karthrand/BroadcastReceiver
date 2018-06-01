4f4336d865e73598d1be6cbc06b47bae192b81ea
Karthrand <735867550@qq.com>
2018-06-01 14:32:57 +0800

    Initial commit

---
+++ app/src/main/java/com/oude/receiver/MainActivity.java
@@ -0,0 +1,14 @@
+package com.oude.receiver;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
