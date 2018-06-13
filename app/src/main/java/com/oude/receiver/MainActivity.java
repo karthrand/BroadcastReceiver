64572d945c0b424963d6d44a39790a0b40eae8bc
Karthrand <735867550@qq.com>
2018-06-14 00:02:45 +0800

    修正接收外部广播

--- app/src/main/java/com/oude/receiver/MainActivity.java
+++ app/src/main/java/com/oude/receiver/MainActivity.java
@@ -9,21 +9,25 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.widget.Toast;
 
-public class MainActivity extends Activity 
+public class MainActivity extends Activity
 {
+
+
     private TextView tv;
-    private String time;
+    private ActiveBroadcastReceiver activeBroadcastReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         tv =findViewById(R.id.mainTextView1);
-        
-        time =getTime();
-        
         tv.setText("广播接收记录:\n");
+        IntentFilter intentFilter =new IntentFilter();
+        intentFilter.addAction("com.oude.broadcast.action.MY_BROADCAST");
+        activeBroadcastReceiver = new ActiveBroadcastReceiver();
+        registerReceiver(activeBroadcastReceiver,intentFilter);
     }
     
     
@@ -33,9 +37,31 @@ public class MainActivity extends Activity
         String str = formatter.format(curDate);
         return str;
     }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        unregisterReceiver(activeBroadcastReceiver);
+    }
     
-    public void setLogs(String log){
-        tv.append(getTitle() + log + "\n");
+    
+
+    
+    class ActiveBroadcastReceiver extends BroadcastReceiver
+    {
+        public String log;
+
+        @Override
+        public void onReceive(Context p1, Intent p2)
+        {
+            Toast.makeText(p1,"外部接收",Toast.LENGTH_SHORT).show();
+            log = "接收到主动广播!";
+            tv.append(getTime() + log + "\n");
+
+        }
+
+
     }
     
 }
